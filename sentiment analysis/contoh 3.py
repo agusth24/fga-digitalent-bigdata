ac5caf199621ca7051e308020051e1e1027a7940
Agus Haryono <54830846+agusth24@users.noreply.github.com>
2022-02-22 20:33:40 +0700

    Update contoh 3.py
    remove token

--- sentiment analysis/contoh 3.py
+++ sentiment analysis/contoh 3.py
@@ -20,10 +20,10 @@ from wordcloud import WordCloud, STOPWORDS
 analyser = SentimentIntensityAnalyzer()
 translator = Translator()
 
-ACCESS_TOKEN = '107282554-vCjzS0Xvdb5h6xnEUYu67mFTUaaUuCPVxCQ2sucd'
-ACCESS_SECRET = 'bt6KXJd6Jfo4qWgoImEZtjiZ7zBk3dF103arwxAUWOljE'
-CONSUMER_KEY = 'u83fQzGVBzGsWbMYEQ3oAwPL1'
-CONSUMER_SECRET = 'rqckM2STaYLQ7RvI3JedImh2tKXeeWzuuzImKObSjAAoApbkkE'
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
